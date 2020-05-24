e772138e16bcab3c6fb50a07785e47ec1bcafefe
vido21 <vidobae@gmail.com>
2020-05-24 23:39:22 +0700

    activity feed

---
+++ app/Observers/ProjectObserver.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Observers;
+
+use App\Project;
+use App\Activity;
+
+class ProjectObserver
+{
+    /**
+     * Handle the project "created" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function created(Project $project)
+    {
+        $this->recordActivity('created', $project);
+    }
+
+    /**
+     * Handle the project "updated" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function updated(Project $project)
+    {
+        $this->recordActivity('updated', $project);
+    }
+
+    /**
+     * Handle the project "deleted" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function deleted(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "restored" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function restored(Project $project)
+    {
+        //
+    }
+
+    /**
+     * Handle the project "force deleted" event.
+     *
+     * @param  \App\Project  $project
+     * @return void
+     */
+    public function forceDeleted(Project $project)
+    {
+        //
+    }
+
+    protected function recordActivity($type, $project)
+    {
+        Activity::create([
+            'project_id' => $project->id,
+            'description' => $type
+        ]);
+    }
+}
--- app/Project.php
+++ app/Project.php
@@ -27,4 +27,9 @@ class Project extends Model
     {
         return $this->hasMany(Task::class);
     }
+
+    public function activity()
+    {
+        return $this->hasMany(Activity::class);
+    }
 }
