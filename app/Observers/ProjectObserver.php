1288cdc63a983b2a75503de0e2b31f2ac7b8b19f
vido21 <vidobae@gmail.com>
2020-05-25 12:19:47 +0700

    activity feeds part2

--- app/Observers/ProjectObserver.php
+++ app/Observers/ProjectObserver.php
@@ -15,7 +15,8 @@ class ProjectObserver
      */
     public function created(Project $project)
     {
-        $this->recordActivity('created', $project);
+        // $this->recordActivity('created', $project);
+        $project->recordActivity('created');
     }
 
     /**
@@ -26,7 +27,8 @@ class ProjectObserver
      */
     public function updated(Project $project)
     {
-        $this->recordActivity('updated', $project);
+        // $this->recordActivity('updated', $project);
+        $project->recordActivity('updated');
     }
 
     /**
@@ -64,9 +66,10 @@ class ProjectObserver
 
     protected function recordActivity($type, $project)
     {
-        Activity::create([
-            'project_id' => $project->id,
-            'description' => $type
-        ]);
+        // Activity::create([
+        //     'project_id' => $project->id,
+        //     'description' => $type
+        // ]);
+        $project->recordActivity('updated');
     }
 }
--- app/Task.php
+++ app/Task.php
@@ -10,6 +10,18 @@ class Task extends Model
 
     protected $touches = ['project'];
 
+    /**
+     * Boot the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($task) {
+            $task->project->recordActivity('created_task');
+        });
+    }
+
     public function path()
     {
         return "/projects/{$this->project->id}/tasks/{$this->id}";
@@ -18,5 +30,12 @@ class Task extends Model
     public function project()
     {
         return $this->belongsTo('App\Project');
+    }
+
+    public function complete()
+    {
+        $this->update(['completed' => true]);
+
+        $this->project->recordActivity('completed_task');
     }
 }
